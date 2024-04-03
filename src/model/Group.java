a6f64c121b554f00ebb9e87707fb65419debf70f
meeror <mihazoed@gmail.com>
2024-04-03 16:30:26 +0300

    Implemented createTypicalUniversity for creating university

--- src/model/Group.java
+++ src/model/Group.java
@@ -4,6 +4,13 @@ import java.util.List;
 import java.util.Objects;
 
 public class Group  {
+    private String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
     private Human head;
 
     public Human getHead() {
